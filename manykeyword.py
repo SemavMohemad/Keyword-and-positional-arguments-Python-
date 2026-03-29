ac501fab017f1f89f2e9a4ef6696030e01d17921
SemavMohemad <semavmh12@gmail.com>
2026-03-29 17:12:00 +0300

    modify two files

--- manykeyword.py
+++ manykeyword.py
@@ -3,12 +3,12 @@ def print_profile(**kwargs):
         print(f"{key}: {value}")
 
 
-print_profile(name="Geeta", field="Computing", award="Gold Medal")
-
-
-def print_profile(**kwargs):
-    for key, value in kwargs.items():
-        print(f"{key}: {value}")
+def print_dict(**kwargs):
+    print(
+        (list(kwargs))[1]
+    )  # -> It's will print as a dictionary {'name': 'Geeta', 'field': 'Computing', 'award': 'Gold Medal'}
 
 
 print_profile(name="Geeta", field="Computing", award="Gold Medal")
+# If i want to extract a specify key
+print_dict(name="Geeta", field="Computing", award="Gold Medal")
